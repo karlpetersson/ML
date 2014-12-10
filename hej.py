3ab3f7db64a300562dc186cff08b6f003e3ecada
karlpetersson <karl.m.petersson@gmail.com>
2014-12-10 19:10:46 +0100

    cleaned dir

--- hej.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-sizes = [2, 3, 1]
-weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-nabla_w = [np.zeros(w.shape) for w in weights]
-print weights
-print nabla_w
