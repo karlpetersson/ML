df4089cbc4d02529281a3bd59fb22c89a7bf9069
karl.m.petersson@gmail.com <karl.m.petersson@gmail.com>
2014-12-19 14:33:17 +0100

    changed to spaces indentation

--- knn.js
+++ knn.js
@@ -1,124 +1,124 @@
 (function () {
 
-	var _ = require('./lib/lodash');
-	var $m = require('./math');
+    var _ = require('./lib/lodash');
+    var $m = require('./math');
 
-	var _knn = {};
+    var _knn = {};
 
-	var DIM_MISMATCH = 'Dimension mismatch between points';
+    var DIM_MISMATCH = 'Dimension mismatch between points';
 
-	function Node(value, label, leftChild, rightChild) {
-		this.value = value;
-		this.label = label;
-		this.leftChild = leftChild;
-		this.rightChild = rightChild;
-	}
+    function Node(value, label, leftChild, rightChild) {
+        this.value = value;
+        this.label = label;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
 
-	function getMaxIdx (list, k) {
-		var idx = 0,
-			max = 0,
-			len = list.length;
+    function getMaxIdx (list, k) {
+        var idx = 0,
+            max = 0,
+            len = list.length;
 
-		if(len < k) {
-			list[len] = Infinity;
-			return len;
-		}
+        if(len < k) {
+            list[len] = Infinity;
+            return len;
+        }
 
-		for(var i = 0; i < len; i++) {
-			if(list[i] > max) {
-				max = list[i];
-				idx = i;
-			}
-		}
+        for(var i = 0; i < len; i++) {
+            if(list[i] > max) {
+                max = list[i];
+                idx = i;
+            }
+        }
 
-		return idx;
-	}
+        return idx;
+    }
 
-	function kdTree (points, depth) {
-		
-		if(!points[0]) {
-			return null;
-		}
+    function kdTree (points, depth) {
+        
+        if(!points[0]) {
+            return null;
+        }
 
-		var k = points[0].x.length;
-		var axis = depth % k;
+        var k = points[0].x.length;
+        var axis = depth % k;
 
-		points = _.sortBy(points, function (p) {
-			return p.x[axis];
-		});
+        points = _.sortBy(points, function (p) {
+            return p.x[axis];
+        });
 
-		var m = Math.floor(points.length / 2);
+        var m = Math.floor(points.length / 2);
 
-		return new Node(
-			points[m].x, points[m].y,
-			kdTree(points.slice(0, m), depth + 1),
-			kdTree(points.slice(m + 1), depth + 1)
-		);
-	}
+        return new Node(
+            points[m].x, points[m].y,
+            kdTree(points.slice(0, m), depth + 1),
+            kdTree(points.slice(m + 1), depth + 1)
+        );
+    }
 
 
-	function init (points) {
-		return kdTree(points, 0);
-	}
+    function init (points) {
+        return kdTree(points, 0);
+    }
 
-	function classify (root, point, k) {
-		var bestGuesses = [],
-			labels = [Infinity];
+    function classify (root, point, k) {
+        var bestGuesses = [],
+            labels = [Infinity];
 
-		// Nearest neighbours search
-		(function next (node, depth) {
-			if(!node) return;
+        // Nearest neighbours search
+        (function next (node, depth) {
+            if(!node) return;
 
-			var axis = depth % point.length;
-			var otherBranch = null;
+            var axis = depth % point.length;
+            var otherBranch = null;
 
-			if(point[axis] < node.value[axis]) {
-				next(node.leftChild, depth + 1);
-				otherBranch = node.rightChild;
-			} else {
-				next(node.rightChild, depth + 1);
-				otherBranch = node.leftChild;
-			}
+            if(point[axis] < node.value[axis]) {
+                next(node.leftChild, depth + 1);
+                otherBranch = node.rightChild;
+            } else {
+                next(node.rightChild, depth + 1);
+                otherBranch = node.leftChild;
+            }
 
-			var idx = getMaxIdx(bestGuesses, k);
-			var dist = $m.euclidianDistance(node.value, point);
+            var idx = getMaxIdx(bestGuesses, k);
+            var dist = $m.euclidianDistance(node.value, point);
 
-			if (dist < bestGuesses[idx]) {
-				bestGuesses[idx] = dist;
-				labels[idx] = node.label;
-			}
+            if (dist < bestGuesses[idx]) {
+                bestGuesses[idx] = dist;
+                labels[idx] = node.label;
+            }
 
-			if (Math.abs(point[axis] - node.value[axis]) < bestGuesses[idx]) {
-				next(otherBranch, depth + 1);
-			}
+            if (Math.abs(point[axis] - node.value[axis]) < bestGuesses[idx]) {
+                next(otherBranch, depth + 1);
+            }
 
-			return;
+            return;
 
-		})(root, 0);
+        })(root, 0);
 
-		var votes = {};
-		for(var i = 0, len = labels.length; i < len; i++) {
-			if(!votes[labels[i]]) {
-				votes[labels[i]] = 0;
-			}
-			votes[labels[i]]++;
-		}
+        var votes = {};
+        for(var i = 0, len = labels.length; i < len; i++) {
+            if(!votes[labels[i]]) {
+                votes[labels[i]] = 0;
+            }
+            votes[labels[i]]++;
+        }
 
-		var best = null;
-		for(var vote in votes) {
-			if(!best || votes[vote] > votes[best]) {
-				best = vote;
-			}
-		}
+        var best = null;
+        for(var vote in votes) {
+                if(!best || votes[vote] > votes[best]) {
+                best = vote;
+            }
+        }
 
-		return best;
-	}
+        return best;
+    }
 
-	_knn.kdTree = kdTree;
-	_knn.init = init;
-	_knn.classify = classify;
+    _knn.kdTree = kdTree;
+    _knn.init = init;
+    _knn.classify = classify;
 
-	module.exports = _knn;
+    module.exports = _knn;
 
 }.call(this));
 
